16e812f734933ff9a4e158891869606b957ffe2f
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-01-12 18:03:31 +0530

    Update AgeCounter.jsx

--- src/AgeCounter.jsx
+++ src/AgeCounter.jsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 
 export function AgeCounter(props) {
   //   let age = 28;
-  //  variables are not reactive, so we need to use states  instead
-  
+  //  variables are not reactive, so we need to use states instead
+
   const [age, setAge] = useState(28);
 
+ // setAge(age); --> dont use setState directly inside the component body else it will result in infinite rendering loop
+
   function increaseAge() {
-    setAge(age + 1);
+    setAge(age + 1); //is asynchronous..will take some time to see the updated value
   }
+
+  console.log("Updated value", age);
   return (
     <div>
       <button onClick={increaseAge}>Click here to increase the age</button>
